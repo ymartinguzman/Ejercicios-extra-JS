c2ddf5b9775b552673b17d609914c6db00638be8
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-16 11:40:10 +0100

    add array

--- main.js
+++ main.js
@@ -9,4 +9,32 @@ function getRandomNumber(max) {
 
 console.log(writeThis('Patata ') + getRandomNumber(10));
 console.log(writeThis('Aguacate ') + getRandomNumber(10));
-console.log(writeThis('Pizza ') + +getRandomNumber(10));
+console.log(writeThis('Pizza ') + getRandomNumber(10));
+
+const myWordList = [
+  {
+    text: 'Pencil',
+    total: 6,
+  },
+  {
+    text: 'Thermo',
+    total: 2,
+  },
+  {
+    text: 'TV',
+    total: 8,
+  },
+  {
+    text: 'Phone',
+    total: 4,
+  },
+];
+
+// let list = '';
+
+// function writeMyArray([]) {
+//   for (let i = 0; i < list.length; i++) {
+//     console.log(writeThis[i].text.myWordList[i].total);
+//   }
+// }
+//writeMyArray();
