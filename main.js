57dc5f6cb65a717716bbab5e39ea47a591098e78
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-16 10:24:59 +0100

    set Fase/1

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 'use strict';
 
-<<<<<<< HEAD
-const ten = 'patata,';
-console.log(ten.repeat(10));
-=======
 function ten() {
   return 'patata';
 }
 
 console.log('patata '.repeat(10));
->>>>>>> master
