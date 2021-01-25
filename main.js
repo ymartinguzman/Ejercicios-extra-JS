fc269f98ffb45954481ff7d260e485547c63f042
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-25 09:40:58 +0100

    add fetch

--- main.js
+++ main.js
@@ -11,31 +11,28 @@ console.log(writeThis('Patata ') + getRandomNumber(10));
 console.log(writeThis('Aguacate ') + getRandomNumber(10));
 console.log(writeThis('Pizza ') + getRandomNumber(10));
 
-const myWordList = [
-  {
-    text: 'Pencil',
-    total: 6,
-  },
-  {
-    text: 'Thermo',
-    total: 2,
-  },
-  {
-    text: 'TV',
-    total: 8,
-  },
-  {
-    text: 'Phone',
-    total: 4,
-  },
-];
+let myWordList = [];
+
+function getInfoApi(arr) {
+  return fetch(
+    `https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      myWordList = data;
+      console.log(data.results);
+    });
+}
+console.log(getInfoApi(myWordList));
 
 function writeMyArray(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let x = 0; x < array[i].total; x++) {
-      writeThis(array[i].text);
+  for (let i = 0; i < myWordList.length; i++) {
+    for (let x = 0; x < myWordList[i].total; x++) {
+      writeThis(myWordList[i].text);
     }
   }
 }
 
-writeMyArray(myWordList);
+//console.log(getInfoApi(myWordList));
+
+writeMyArray();
