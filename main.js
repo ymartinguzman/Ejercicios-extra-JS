48eafa911c1ef3bb35da1b765af0e341c2aa8260
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-25 10:32:08 +0100

    update fase5 with an array

--- main.js
+++ main.js
@@ -7,32 +7,35 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log(writeThis('Patata ') + getRandomNumber(10));
-console.log(writeThis('Aguacate ') + getRandomNumber(10));
-console.log(writeThis('Pizza ') + getRandomNumber(10));
+// console.log(writeThis('Patata ') + getRandomNumber(10));
+// console.log(writeThis('Aguacate ') + getRandomNumber(10));
+// console.log(writeThis('Pizza ') + getRandomNumber(10));
 
-let myWordList = [];
-
-function getInfoApi(arr) {
-  return fetch(
-    `https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      myWordList = data;
-      console.log(data.results);
-    });
-}
-console.log(getInfoApi(myWordList));
+const myWordList = [
+  {
+    text: 'Pencil',
+    total: 6,
+  },
+  {
+    text: 'Thermo',
+    total: 2,
+  },
+  {
+    text: 'TV',
+    total: 8,
+  },
+  {
+    text: 'Phone',
+    total: 4,
+  },
+];
 
 function writeMyArray(array) {
-  for (let i = 0; i < myWordList.length; i++) {
-    for (let x = 0; x < myWordList[i].total; x++) {
-      writeThis(myWordList[i].text);
+  for (let i = 0; i < array.length; i++) {
+    for (let x = 0; x < array[i].total; x++) {
+      writeThis(array[i].text);
     }
   }
 }
 
-//console.log(getInfoApi(myWordList));
-
-writeMyArray();
+console.log(writeMyArray(myWordList));
