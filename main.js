ed5e40c4fa803b36723b6abd57efb114e9aaa300
ymartinguzman <yamiramartinguzman@gmail.com>
2021-01-25 12:28:07 +0100

    set Fase/6

--- main.js
+++ main.js
@@ -7,35 +7,24 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log(writeThis('Patata ') + getRandomNumber(10));
-console.log(writeThis('Aguacate ') + getRandomNumber(10));
-console.log(writeThis('Pizza ') + getRandomNumber(10));
+let myWordList = [];
 
-const myWordList = [
-  {
-    text: 'Pencil',
-    total: 6,
-  },
-  {
-    text: 'Thermo',
-    total: 2,
-  },
-  {
-    text: 'TV',
-    total: 8,
-  },
-  {
-    text: 'Phone',
-    total: 4,
-  },
-];
+function getInfoApi(arr) {
+  return fetch(
+    `https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      myWordList = data.results;
+      console.log(myWordList);
+      writeMyArray();
+    });
+}
 
-function writeMyArray(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let x = 0; x < array[i].total; x++) {
-      writeThis(array[i].text);
-    }
+function writeMyArray() {
+  for (let i = 0; i < myWordList.length; i++) {
+    console.log(myWordList[i].text, myWordList[i].total);
   }
 }
 
-writeMyArray(myWordList);
+getInfoApi(myWordList);
